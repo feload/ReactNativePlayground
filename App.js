e2ba67ebacf2fa3e94a31a15345f83e0f6158a29
Felipe <feload@gmail.com>
2018-05-12 19:14:08 -0500

    Install React-Navigator and add an ufly fix to ignore yellow message...

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 import Header from './src/components/Header';
 
+// This is a pretty ugly "fix".
+// Seems like by the time I'm writing this, react navigation guys are having some kind of problems.
+// So, the only option they give is to ignore this message.
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
-export default App = () => (
-  <View>
-    <Header />
-    <Text>Hello :D</Text>
-  </View>
-);
+class HomeScreen extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Home screen</Text>
+      </View>
+    )
+  }
+}
+
+export default createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+});
