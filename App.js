087d74f2289ef65628b3038156df0c6b9f7f0f63
Felipe <feload@gmail.com>
2018-05-13 07:19:21 -0500

    Add glamorous library.

--- App.js
+++ App.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-import HomeScreen  from './src/components/HomeScreen';
-import DetailsScreen from './src/components/DetailsScreen';
-
-// This is a pretty ugly "fix".
-// Seems like by the time I'm writing this, react navigation guys are having some kind of problems.
-// So, the only option they give is to ignore this message.
-import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
-
-const routerOptions = {
-  initialRouteName: 'Home'
-}
-
-const RootStack = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  },
-  Details: {
-    screen: DetailsScreen
-  }
-}, routerOptions);
+import { View, Text } from 'react-native';
+import Header from './src/components/Header';
 
 export default class App extends Component {
   render () {
-    return <RootStack />
+    return (
+      <View>
+        <Header title="Albums" />
+      </View>
+    )
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import glamorous from 'glamorous-native';
 
-export default ({ title }) => {
-  return (
-    <View>
-      <Text>{ title }</Text>
-    </View>
-  )
+const StyledText = glamorous.text({
+  fontSize: 20
+});
+
+export default class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    const { title } = this.props;
+
+    return (
+      <View>
+        <StyledText>{ title }</StyledText>
+      </View>
+    )
+  }
 }
