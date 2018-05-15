53ec05ba6f1d972a89e59167310a2420805222f1
Felipe <fj.lopez@ugto.mx>
2018-05-15 08:21:22 -0500

    Add firebase to the mix.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Header from './components/Header';
 import AlbumList from './components/AlbumList';
 import albums from './data/music_albums.json';
 
+import firebase from 'firebase';
+import firebaseConf from './config/firebase';
+firebase.initializeApp(firebaseConf);
+
 export default class App extends Component {
+  componentWillMount () {}
   render () {
     return (
       <View>
---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyBODGqim_Q2Rv7YJPhMEsfTo3n71HYDcRA",
+  authDomain: "manager-7933d.firebaseapp.com",
+  databaseURL: "https://manager-7933d.firebaseio.com",
+  projectId: "manager-7933d",
+  storageBucket: "manager-7933d.appspot.com",
+  messagingSenderId: "905698267071"
+};
